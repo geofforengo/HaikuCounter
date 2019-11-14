384a908080330b85004e649f2cc07d61ba03b759
Student <example@example.com>
2019-11-14 15:29:31 -0800

    add function to test

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -3,15 +3,21 @@ import { Haiku } from './../src/haiku.js';
 describe('Haiku', () => {
 
   test('should determine if all inputs have values entered', () => {
-    let haiku = new Haiku("words","1234","words");
-    expect(haiku.line1).toEqual("words");
-    expect(haiku.line2).toEqual("1234");
-    expect(haiku.line3).toEqual("words");
+    let haiku = new Haiku("words","","words");
+    expect(haiku.checkFilled()).toEqual("Please enter words for every line");
   });
-  test('Program checks that only letters and punctuation are inputted', () => {
-    let haiku = new Haiku("words","words","words");
-    expect(haiku.line1).toEqual("words");
-    expect(haiku.line2).toEqual("words");
-    expect(haiku.line3).toEqual("words");
-  });
+  // test('Program checks that only letters and punctuation are inputted', () => {
+  //   let notHaiku = new Haiku("1-2;","words","words");
+  //   let line1 = notHaiku.line1
+  //   expect(line1.checkFilled()).toEqual("Please use letters only");
+  //   // expect(haiku.line2).toEqual("words");
+  //   // expect(haiku.line3).toEqual("words");
+  // });
+  // test('Program checks that only letters and punctuation are inputted', () => {
+  //   let haiku = new Haiku("words and","words and","words");
+  //   expect(haiku.line1).toEqual("words" "and");
+  //   expect(haiku.line2).toEqual("words and");
+  //   expect(haiku.line3).toEqual("words");
+  // });
+
 });
--- src/haiku.js
+++ src/haiku.js
@@ -9,15 +9,13 @@ export class Haiku {
     if ((this.line1 === "") || (this.line2 === "") || (this.line3 === "")) {
       return "Please enter words for every line";
     }
-    else if (this.line1.inclues(/\W/)) {
-      return "Please use letters only"
-    }
-  }
-
-  toStrings() {
-    let line1 = this.line1.split(' ');
-    console.log(line1);
-    let line2 = this.line2.split(' ');
-    let line3 = this.line3.split(' ');
+    // else if (this.line1.inclues(/\W/)) {
+    //   return "Please use letters only"
+    // }
   }
 }
+//   toStrings() {
+//     let line1 = this.line1.split(' ');
+//     let line2 = this.line2.split(' ');
+//     let line3 = this.line3.split(' ');
+//   }
