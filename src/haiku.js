3b63160d2f99881ea7319bdbc8264eb451256463
Student <example@example.com>
2019-11-14 14:36:07 -0800

    add else if statement

--- src/haiku.js
+++ src/haiku.js
@@ -1,13 +1,16 @@
 export class Haiku {
   constructor(line1, line2, line3) {
-    this.line1 = line1;
-    this.line2 = line2;
-    this.line3 = line3;
+    this.line1 = line1,
+    this.line2 = line2,
+    this.line3 = line3
   }
 
   checkFilled() {
     if ((this.line1 === "") || (this.line2 === "") || (this.line3 === "")) {
       return "Please enter words for every line";
-    };
-  };
-};
+    }
+    else if (this.line1.inclues(/\W/)) {
+      return "Please use letters only"
+    }
+  }
+}
